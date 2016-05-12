13602f5763f03bad86d465edf6bf08e4b3078c6b
Carson Robles <carson.robles@gmail.com>
2016-05-12 14:12:31 -0700

    reorganized keypad.ino

---
+++ src/keypad.h
@@ -0,0 +1,30 @@
+
+#ifndef KEYPAD_H
+
+#define KEYPAD_H
+
+// number of digits in code
+#define CODE_LEN 4
+
+// define pins associated with buttons
+#define BUT_1_PIN 2    // prp
+#define BUT_2_PIN 3    // blu
+#define BUT_3_PIN 4    // grn
+#define BUT_4_PIN 5    // ylw
+#define ENTER_PIN 6    // org
+
+// define pins for limit switches
+#define LOCKED_LIMIT   7
+#define UNLOCKED_LIMIT 8
+
+// define other pins
+#define LED_PIN   13
+#define SERVO_PIN 12
+
+// define servo attributes
+#define UNLOCK_TIME            3000
+#define SERVO_STOP             91
+#define SERVO_CLOCK_WISE       30
+#define SERVO_COUNT_CLOCK_WISE 150
+
+#endif
--- src/keypad.ino
+++ src/keypad.ino
@@ -1,32 +1,10 @@
 // include Arduino servo library
 #include <Servo.h>
 
-// number of digits in code
-#define CODE_LEN 4
+#include "keypad.h"
 
 // code
 const char CODE[CODE_LEN] = {1, 2, 3, 4};
-
-// define pins associated with buttons
-#define BUT_1_PIN 2    // prp
-#define BUT_2_PIN 3    // blu
-#define BUT_3_PIN 4    // grn
-#define BUT_4_PIN 5    // ylw
-#define ENTER_PIN 6    // org
-
-// define pins for limit switches
-#define LOCKED_LIMIT   7
-#define UNLOCKED_LIMIT 8
-
-// define other pins
-#define LED_PIN   13
-#define SERVO_PIN 12
-
-// define servo attributes
-#define UNLOCK_TIME            3000
-#define SERVO_STOP             91
-#define SERVO_CLOCK_WISE       30
-#define SERVO_COUNT_CLOCK_WISE 150
 
 // servo to open door
 Servo servo;
@@ -97,7 +75,7 @@ void loop(void) {
     if (checkCode()) {
       resetCode();
       digitalWrite(LED_PIN, HIGH);
-      
+
       servo.write(SERVO_CLOCK_WISE);
 
       do
@@ -120,7 +98,7 @@ void loop(void) {
 
 bool checkCode(void) {
   int i;
-  
+
   for (i = 0; i < CODE_LEN; i++)
     if (usrCode[i] != CODE[CODE_LEN - (i + 1)])
       return (false);
