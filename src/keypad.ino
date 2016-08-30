0fa040c580d8c281b92f88089d702862f7a7f1ef
Carson Robles <carson.robles@gmail.com>
2016-08-29 17:39:37 -0700

    reorganize

--- src/keypad.ino
+++ src/keypad.ino
@@ -15,125 +15,109 @@ int usrCode[CODE_LEN];
 
 /* initialization */
 void setup(void) {
-  /* set pin modes for all used pins */
-  pinMode(BUT_1_PIN, INPUT);
-  pinMode(BUT_2_PIN, INPUT);
-  pinMode(BUT_3_PIN, INPUT);
-  pinMode(BUT_4_PIN, INPUT);
-  pinMode(ENTER_PIN, INPUT);
+    /* set pin modes for all used pins */
+    pinMode(BUT_1_PIN,      INPUT);
+    pinMode(BUT_2_PIN,      INPUT);
+    pinMode(BUT_3_PIN,      INPUT);
+    pinMode(BUT_4_PIN,      INPUT);
+    pinMode(ENTER_PIN,      INPUT);
+    pinMode(LOCKED_LIMIT,   INPUT);
+    pinMode(UNLOCKED_LIMIT, INPUT);
 
-  pinMode(LOCKED_LIMIT, INPUT);
-  pinMode(UNLOCKED_LIMIT, INPUT);
+    pinMode(LED_PIN, OUTPUT);
 
-  pinMode(LED_PIN, OUTPUT);
+    /* attach servo to its pin */
+    servo.attach(SERVO_PIN);
 
-  /* attach servo to its pin */
-  servo.attach(SERVO_PIN);
-
-  /* set usrCode to all zeroes */
-  resetCode();
-
-  /* start up Serial connection */
-  //Serial.begin(9600);
-
-  /* wait for Serial to connect */
-  /*while (!Serial);*/
-
-  /* notify that initialization has completed */
-  /*Serial.println("[init done]");*/
+    /* set usrCode to all zeroes */
+    resetCode();
 }
 
 /* continuously called */
 void loop(void) {
-  /*char str[20];*/
+    servoStop();
 
-  /*sprintf(str, "locked, unlocked: %d, %d\n", digitalRead(LOCKED_LIMIT), digitalRead(UNLOCKED_LIMIT));*/
-  /*Serial.println(str);*/
-  /*printCode();*/
+    /*servo.write(LOCKED_POS);*/
 
-  servoStop();
+    /* check if button 1 was pressed */
+    checkPin(BUT_1_PIN);
 
-  /*servo.write(LOCKED_POS);*/
+    /* check if button 2 was pressed */
+    checkPin(BUT_2_PIN);
 
-  /* check if button 1 was pressed */
-  checkPin(BUT_1_PIN);
+    /* check if button 3 was pressed */
+    checkPin(BUT_3_PIN);
 
-  /* check if button 2 was pressed */
-  checkPin(BUT_2_PIN);
+    /* check if button 4 was pressed */
+    checkPin(BUT_4_PIN);
 
-  /* check if button 3 was pressed */
-  checkPin(BUT_3_PIN);
+    /* check if enter button was pressed */
+    if (digitalRead(ENTER_PIN) == HIGH) {
+        do
+            delay(10);
+        while (digitalRead(ENTER_PIN) == HIGH);
 
-  /* check if button 4 was pressed */
-  checkPin(BUT_4_PIN);
+        if (checkCode()) {
+            resetCode();
+            digitalWrite(LED_PIN, HIGH);
 
-  /* check if enter button was pressed */
-  if (digitalRead(ENTER_PIN) == HIGH) {
-    do
-      delay(10);
-    while (digitalRead(ENTER_PIN) == HIGH);
+            servo.write(SERVO_CLOCK_WISE);
 
-    if (checkCode()) {
-      resetCode();
-      digitalWrite(LED_PIN, HIGH);
+        do
+            delay(10);
+        while (digitalRead(UNLOCKED_LIMIT) == LOW);
 
-      servo.write(SERVO_CLOCK_WISE);
+        servoStop();
+        delay(UNLOCK_TIME);
 
-      do
-        delay(10);
-      while (digitalRead(UNLOCKED_LIMIT) == LOW);
+        servo.write(SERVO_COUNT_CLOCK_WISE);
 
-      servoStop();
-      delay(UNLOCK_TIME);
+        do
+            delay(10);
+        while (digitalRead(LOCKED_LIMIT) == LOW);
 
-      servo.write(SERVO_COUNT_CLOCK_WISE);
-
-      do
-        delay(10);
-      while (digitalRead(LOCKED_LIMIT) == LOW);
-
-      digitalWrite(LED_PIN, LOW);
+        digitalWrite(LED_PIN, LOW);
     }
   }
 }
 
 bool checkCode(void) {
-  int i;
+    int i;
 
-  for (i = 0; i < CODE_LEN; i++)
-    if (usrCode[i] != CODE[CODE_LEN - (i + 1)])
-      return (false);
+    for (i = 0; i < CODE_LEN; i++)
+        if (usrCode[i] != CODE[CODE_LEN - (i + 1)])
+            return (false);
 
-  return (true);
+    return (true);
 }
 
 void checkPin(int pin) {
-  if (digitalRead(pin) == HIGH) {
-    pushOnCode(pin - 1);
+    if (digitalRead(pin) == HIGH) {
+        pushOnCode(pin - 1);
 
-    /* waits for pin to go low */
-    do
-      delay(10);
-    while (digitalRead(pin) == HIGH);
-  }
+        /* waits for pin to go low */
+        do
+            delay(10);
+        while (digitalRead(pin) == HIGH);
+    }
 }
 
 void pushOnCode(int n) {
-  int i;
+    int i;
 
-  for (i = CODE_LEN - 1; i > 0; i--)
-    usrCode[i] = usrCode[i - 1];
+    for (i = CODE_LEN - 1; i > 0; i--)
+        usrCode[i] = usrCode[i - 1];
 
-  usrCode[0] = n;
+        usrCode[0] = n;
 }
 
 void resetCode(void) {
-  int i;
+    int i;
 
-  for (i = 0; i < CODE_LEN; i++)
-    usrCode[i] = 0;
+    for (i = 0; i < CODE_LEN; i++)
+        usrCode[i] = 0;
 }
 
 void servoStop(void) {
-  servo.write(SERVO_STOP);
+    servo.write(SERVO_STOP);
 }
